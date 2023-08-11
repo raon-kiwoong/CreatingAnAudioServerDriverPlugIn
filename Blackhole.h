ad74b11c037125151da9fb0e70735852e598dc0f
kiwoongkim <kiwoongkim@raondata.ai>
2023-08-11 10:59:57 +0900

    1st commit

---
+++ Blackhole.h
@@ -0,0 +1,14 @@
+//
+//  Blackhole.h
+//  NullAudio
+//
+//  Created by 김기웅 on 2023/07/31.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+#ifndef Blackhole_h
+#define Blackhole_h
+
+#include <stdio.h>
+
+#endif /* Blackhole_h */
